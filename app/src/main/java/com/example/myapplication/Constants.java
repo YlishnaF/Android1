62ffadb845446274483bb05bb56d46a5e204337b
YlishnaF <ylishnaf@yandex.ru>
2020-10-15 17:33:47 +0300

    4th lession

---
+++ app/src/main/java/com/example/myapplication/Constants.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+public interface Constants {
+    static final String LOCATION = "LOCATION";
+    static final int REQUEST_CODE = 5;
+    static final String LOCATION_KEY = "LOCATION_KEY";
+
+ }
